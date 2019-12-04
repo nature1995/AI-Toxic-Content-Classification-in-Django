5c5ed451c54b31dd37f60900f0e95416ee46c528
Ubuntu <ubuntu@ip-172-31-44-178.us-east-2.compute.internal>
2019-12-04 01:46:12 +0000

    fix: tensorflow_core.keras bug

--- Web/apps/identification/__init__.py
+++ Web/apps/identification/__init__.py
@@ -2,11 +2,10 @@ import os
 import tensorflow as tf
 import keras.backend.tensorflow_backend as KTF
 
-# 指定第一块GPU可用 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 config = tf.ConfigProto()
-config.gpu_options.allow_growth=True   #不全部占满显存, 按需分配
+confie.gpu_options.allow_growth=True 
 sess = tf.Session(config=config)
 
 KTF.set_session(sess)
